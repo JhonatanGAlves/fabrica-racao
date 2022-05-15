9bdd3c6521e65679294e2e062051f767ddc5b51a
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-15 20:16:17 -0300

    🆕 title: Header component created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { Container } from "./styles"
+import logo from "../../assets/images/logo-header.svg"
+
+export const Header = () => {
+  return (
+    <Container>
+      <div className="content-header">
+        <img src={logo} alt="Logo do menu" />
+        <nav>
+          <a className="active" href="#">home</a>
+          <a href="#">sobre nós</a>
+          <a href="#">aves</a>
+          <a href="#">pets</a>
+          <a href="#">bovinos</a>
+          <a href="#">marketing</a>
+        </nav>
+      </div>
+    </Container>
+  )
+}
