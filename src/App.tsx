4c4be30d6d905b2a5508c6f3192772781904e16d
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-15 12:15:21 -0300

    ➕ title: import GlobalStyle component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global"
+
 export const App = () => {
   return (
     <div className="App">
       <div>Hello World!</div>
+      <GlobalStyle />
     </div>
   )
 }
