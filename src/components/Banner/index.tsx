a7fc62a9ca6ba6e102d04557e00ed213d178f326
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-15 20:22:10 -0300

    🆕 title: Banner component created

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,22 @@
+import { Container } from "./style"
+import bgVideo from "../../assets/videos/background-banner-video.mp4"
+
+export const Banner = () => {
+  return (
+    <Container>
+      <video autoPlay loop muted>
+        <source src={bgVideo} type="video/mp4" />
+      </video>
+      <div className="txt-hero">
+        <h1>
+          JEZG com você para a<br />
+          melhor performance.
+        </h1>
+        <h3>
+          Produzimos o que há de melhor em nutrição animal para garantir a eficiência
+          e produtividade dos sistemas agropecuários e aumentar a sustentabilidade do negócio.
+        </h3>
+      </div>
+    </Container>
+  )
+}
